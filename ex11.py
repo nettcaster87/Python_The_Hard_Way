f8635eb00fa8bded06b6c0359f4cf78c05acb786
Khigha87 <nettcaster87@gmail.com>
2021-06-15 21:34:07 +0200

    Commit exercise 11

---
+++ ex11.py
@@ -0,0 +1,19 @@
+print("How old are you?", end=' ')
+age = input()
+print("How tall are you?", end=' ')
+height = input()
+print("How much do you weigh?", end=' ')
+weight = input()
+
+print(f"So, you're {age} years old, {height} meters tall and weigh {weight} kilograms.")
+
+#####################################
+
+print("What is your favourite food?", end=' ')
+food = input()
+print("What is your favourite band?", end=' ')
+band = input()
+print("What is your favourite game?", end=' ')
+game = input()
+
+print(f"You like eating {food} while listening to {band} and playing {game}, intersting.")
